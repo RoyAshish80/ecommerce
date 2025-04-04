0809981324326d09439873e4abaf72ddeed7fe6c
Ashish Roy <58362174+RoyAshish80@users.noreply.github.com>
2025-04-04 21:27:37 +0200

    Full implementation without the styling
    Full implementation without the styling of page components

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -1,19 +1,84 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Product } from "./product-data";
-export default function ProductsList({ products }: { products: Product[] }) {
+
+export default function ProductsList({
+  products,
+  initialCartProducts = [],
+}: {
+  products: Product[];
+  initialCartProducts: Product[];
+}) {
+  const [cartProducts, setCartProducts] = useState(initialCartProducts);
+  async function addToCart(productId: string) {
+    const response = await fetch("http://localhost:3000/api/users/2/cart", {
+      method: "POST",
+      body: JSON.stringify({
+        productId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const updatedCartProducts = await response.json();
+    setCartProducts(updatedCartProducts);
+  }
+  async function removeFromCart(productId: string) {
+    const response = await fetch("http://localhost:3000/api/users/2/cart", {
+      method: "DELETE",
+      body: JSON.stringify({
+        productId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const updatedCartProducts = await response.json();
+    setCartProducts(updatedCartProducts);
+  }
+  function productIsInCart(productId: string) {
+    return cartProducts.some((cp) => cp.id === productId);
+  }
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-8">
       {products.map((product) => (
-        <Link key={product.id} href={"/products/" +product.id}>
-          <Image
-            src={"/" + product.imageUrl}
-            alt="Product Image"
-            width={150}
-            height={150}
-          />
-          <h2>{product.name}</h2>
-          <p>${product.price}</p>
+        <Link
+          key={product.id}
+          href={"/products/" + product.id}
+          className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300"
+        >
+          <div className="flex justify-center mb-4 h-48 relative">
+            <Image
+              src={"/" + product.imageUrl}
+              alt="Product Image"
+              width={150}
+              height={150}
+              className="object-cover rounded-md"
+            />
+          </div>
+          <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+          <p className="text-grey-600">${product.price}</p>
+          {productIsInCart(product.id) ? (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                removeFromCart(product.id);
+              }}
+            >
+              Remove from Cart
+            </button>
+          ) : (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                addToCart(product.id);
+              }}
+            >
+              Add to Cart
+            </button>
+          )}
         </Link>
       ))}
     </div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,16 @@
 import ProductsList from "../ProductsList";
-import {products} from '../product-data';
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  const response = await fetch("http://localhost:3000/api/products");
+  const products = await response.json();
+  const response2 = await fetch("http://localhost:3000/api/users/2/cart", {
+    cache: "no-cache",
+  });
+  const cartProducts = await response2.json();
   return (
     <>
-    <h1>Products</h1>
-    <ProductsList products={products} />
+      <h1>Products</h1>
+      <ProductsList products={products} initialCartProducts={cartProducts} />
     </>
-  )
+  );
 }
